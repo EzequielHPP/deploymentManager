bf0764d1144088a192f915225bda908ea4e64f8d
ezequiel@energyhousedigital.com <ezequiel@energyhousedigital.com>
2017-03-13 18:14:32 +0000

    Added Sites and Settings migration files

---
+++ database/migrations/2017_03_13_180714_create_sites_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateSitesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('sites', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->string('local_url');
+            $table->string('local_path');
+            $table->string('current_commit_local');
+            $table->string('remote_url');
+            $table->string('remote_path');
+            $table->string('current_commit_remote');
+            $table->string('deployment_branch');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('sites');
+    }
+}

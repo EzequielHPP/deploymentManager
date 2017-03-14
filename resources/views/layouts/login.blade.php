0d58aaba0dfdf215a06efc12c1d9dc960e49bdf3
ezequiel@energyhousedigital.com <ezequiel@energyhousedigital.com>
2017-03-14 10:08:18 +0000

    Login and register roots added

---
+++ resources/views/layouts/login.blade.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ezequielpereira
+ * Date: 13/03/2017
+ * Time: 18:21
+ */
+?>
+<!DOCTYPE html>
+<html lang="{{ config('app.locale') }}">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Migration manager</title>
+
+    <!-- Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+</head>
+<body>
+<div class="flex-center position-ref full-height">
+
+</div>
+</body>
+</html>
+
